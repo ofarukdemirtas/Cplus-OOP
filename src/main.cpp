d00b5c7b74a962db1f54ff8ec414cda2eb17403c
Ömer Faruk Demirtaş <80536025+ofarukdemirtas@users.noreply.github.com>
2023-07-03 15:26:07 +0100

    deconstructor

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,9 @@ int main(){
     p=&car1;
     p-> ruhsat();
     delete p ; // if u create a class with pointer u should delet it after u use !!!
-     cout<<car1.getcarcolor();
-    
+    cout<<car1.getcarcolor();
+    cout<<car2.getcarcolor();
+
     
    // car2.ruhsat();
    // cout<<endl<<endl;
